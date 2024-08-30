0a883d5ceecc84d9e3c83a4c6ea791651d4f81e0
Mia Ćaćić <cacic.miaa@gmail.com>
2024-08-30 11:07:24 +0200

    feat:router, react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import { createRoot } from 'react-dom/client';
 import SearchParams from './SearchParams';
+import { Link } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Details from './Details';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 const App = () => {
   return (
-    <div>
-      <h1>Adopt me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 };
 const container = document.getElementById('root');
