9109c22a9ecde7915567ab134522ecb902b3d533
Mia Ćaćić <cacic.miaa@gmail.com>
2024-08-30 09:06:32 +0200

    add animals request

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Pet from './Pet';
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
 const SearchParams = () => {
   const [location, setLocation] = useState('');
   const [animal, setAnimal] = useState('');
   const [breed, setBreed] = useState('');
-  const breeds = [];
+  const [pets, setPets] = useState([]);
 
+  const breeds = [];
+  useEffect(() => {
+    requestPets();
+  }, []);
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`,
+    );
+    const json = await res.json();
+    setPets(json.pets);
+  }
   return (
     <div className="search-params">
-      <form action="">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -59,6 +76,10 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+
+      {pets?.map(pet => (
+        <Pet key={pet.id} name={pet.name} animal={pet.animal} breed={pet.breed} />
+      ))}
     </div>
   );
 };
